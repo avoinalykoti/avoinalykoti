9d41846bc0ed8dd45a0666f4ff614ad673b154ea
Tommi Savikko <tommi.savikko@kontakto.fi>
2022-03-24 15:24:33 +0200

    WIP

--- ansible/start.sh
+++ ansible/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 set -eu
 ansible-galaxy install -r requirements.yaml
-ansible-playbook raspi-install-playbook.yaml -i hosts.yaml
+ansible-playbook raspi-install-playbook.yaml -i hosts.yaml -K
